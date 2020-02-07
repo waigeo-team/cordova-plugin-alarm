a113775250b9615a2b57e45bbdbec3315c79404c
Jean Bachelet <j.bachelet@waigeo.fr>
2020-02-07 10:26:07 +0100

    Suppression de la demande de droit d'accès au stockage externe

--- www/alarm.js
+++ www/alarm.js
@@ -2,24 +2,24 @@ var exec = require('cordova/exec');
 
 var PLUGIN_NAME = 'Alarm';
 
-function onDeviceReady() {
-    if (device.platform === "Android") {
-        // request read access to the external storage if we don't have it
-        cordova.plugins.diagnostic.getExternalStorageAuthorizationStatus(function (status) {
-            if (status === cordova.plugins.diagnostic.permissionStatus.GRANTED) {
-                console.log("External storage use is authorized");
-            } else {
-                cordova.plugins.diagnostic.requestExternalStorageAuthorization(function (result) {
-                    console.log("Authorization request for external storage use was " + (result === cordova.plugins.diagnostic.permissionStatus.GRANTED ? "granted" : "denied"));
-                }, function (error) {
-                    console.error(error);
-                });
-            }
-        }, function (error) {
-            console.error("The following error occurred: " + error);
-        });
-    }
-}
+// function onDeviceReady() {
+//     if (device.platform === "Android") {
+//         // request read access to the external storage if we don't have it
+//         cordova.plugins.diagnostic.getExternalStorageAuthorizationStatus(function (status) {
+//             if (status === cordova.plugins.diagnostic.permissionStatus.GRANTED) {
+//                 console.log("External storage use is authorized");
+//             } else {
+//                 cordova.plugins.diagnostic.requestExternalStorageAuthorization(function (result) {
+//                     console.log("Authorization request for external storage use was " + (result === cordova.plugins.diagnostic.permissionStatus.GRANTED ? "granted" : "denied"));
+//                 }, function (error) {
+//                     console.error(error);
+//                 });
+//             }
+//         }, function (error) {
+//             console.error("The following error occurred: " + error);
+//         });
+//     }
+// }
 
 var Alarm = {
     add: function (successCallback, errorCallback, options) {
@@ -42,6 +42,6 @@ var Alarm = {
     }
 };
 
-document.addEventListener("deviceready", onDeviceReady, false);
+// document.addEventListener("deviceready", onDeviceReady, false);
 module.exports = Alarm;
 
